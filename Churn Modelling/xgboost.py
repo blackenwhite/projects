2292e2e717158395dcb4febcd5118856ec850630
Nabajyoti Majumdar <52462493+blackenwhite@users.noreply.github.com>
2019-08-04 12:01:57 +0530

    Update xgboost.py

--- Churn Modelling/xgboost.py
+++ Churn Modelling/xgboost.py
@@ -1,35 +1,34 @@
-# XGBoost
 
-# Install xgboost following the instructions on this link: http://xgboost.readthedocs.io/en/latest/build.html#
 
 # Importing the libraries
-import numpy as np
-import matplotlib.pyplot as plt
-import pandas as pd
+import pandas as p
+import numpy as n
+import matplotlib.pyplot as pl
+
 
 # Importing the dataset
-dataset = pd.read_csv('Churn_Modelling.csv')
+dataset = pd.read_csv('bhola.csv')
 X = dataset.iloc[:, 3:13].values
-y = dataset.iloc[:, 13].values
+y = dataset.iloc[:, 3].values
 
 # Encoding categorical data
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
-labelencoder_X_1 = LabelEncoder()
-X[:, 1] = labelencoder_X_1.fit_transform(X[:, 1])
-labelencoder_X_2 = LabelEncoder()
-X[:, 2] = labelencoder_X_2.fit_transform(X[:, 2])
-onehotencoder = OneHotEncoder(categorical_features = [1])
+labelenco_X_1 = LabelEncoder()
+X[:, 1] = labelenco_X_1.fit_transform(X[:, 1])
+labelencoder_X2 = LabelEncoder()
+X[:, 2] = labelencoder_X2.fit_transform(X[:, 2])
+onehotencoder = OneHotEncoder(categorical_feature = [1])
 X = onehotencoder.fit_transform(X).toarray()
 X = X[:, 1:]
 
 # Splitting the dataset into the Training set and Test set
 from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.4, random_state = 1)
 
 # Fitting XGBoost to the Training set
 from xgboost import XGBClassifier
-classifier = XGBClassifier()
-classifier.fit(X_train, y_train)
+clas = XGBClassifier()
+clas.fit(X_train, y_train)
 
 # Predicting the Test set results
 y_pred = classifier.predict(X_test)
@@ -40,6 +39,4 @@ cm = confusion_matrix(y_test, y_pred)
 
 # Applying k-Fold Cross Validation
 from sklearn.model_selection import cross_val_score
-accuracies = cross_val_score(estimator = classifier, X = X_train, y = y_train, cv = 10)
-accuracies.mean()
-accuracies.std()
+accuracies = cross_val_sco(estimator = classifier, X = X_train, y = y_train, cv = 10)
