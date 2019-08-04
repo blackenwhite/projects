06f8b6fff675e14979073afdb6527191a76ef092
Nabajyoti Majumdar <52462493+blackenwhite@users.noreply.github.com>
2019-08-04 12:55:14 +0530

    Update 1.py

--- Game of pets/1.py
+++ Game of pets/1.py
@@ -1,66 +1,64 @@
-#class pet implementation
-from random import randrange
 
-p1=Pet("Fid")
-print(p1)
+
+
+p=P("Fid")
+print(p)
 for i in range(4):
-    p1.clock_tick()
-    print(p1)
-p1.feed()
-p1.hi()
-p1.teach("Boo")
-for i in range(10):
-    p1.hi()
+    p.clock_tick()
+    print(p)
+p.feedo()
+p.hig()
+p.oteach("Boo")
+for i in range(2):
+    p.hig()
 print(p1)
 
-class Pet(object):
-    boredom_dec=4
-    hunger_dec=6
-    boredom_thr=5
-    hunger_thre=10
-    sounds=['Mrr']
-    def __init__(self,name="Bitty"):
-        self.name=name
-        self.hunger=randrange(self.hunger_threshold)
-        self.boredom=randrange(self.boredom_threshold)
-        self.sounds=self.sounds[:] #copy the class attribute so that  we can make changes
-                                   #to it, we wont afffect the other pets in the class
+class P(object):
+    bore=4
+    hun=6
+    bored=5
+    hunge=10
+    sound=['Mrr']
+    def __init__(self,nam="Bitty"):
+        self.name=nam
+        self.hunger=randrang(self.hunger_threshold)
+        self.boredom=randrang(self.boredom_threshold)
+        self.sounds=self.sound[:] 
     def clock_tick(self):
-            self.boredom+=1
-            self.hunger+=1
+            self.bore+=1
+            self.hun+=1
             
-    def mood(self):
-            if self.hunger<=self.hunger_threshold and self.boredom<=self.boredom_threshold:
-                return "happy"
-            elif self.hunger > self.hunger_threshold:
-                return "hungry"
+    def moodi(self):
+            if self.hun<=self.hung and self.bore<=self.bored:
+                return "happyy"
+            elif self.hun > self.hun:
+                return "hun"
             else:
-                return "bored"
+                return "bor"
     def __str__(self):
-            state="   I'm "+self.name + ". "
-            state+="I feel " +self.mood()+". "
-            #state+="Hunger {} Boredom {} Words {} ".format(self.hunger,self.boredom,self.sounds)
-            return state
+            istat="   I'm "+self.name + ". "
+            istate+="I feel " +self.mood()+". "
+            
+            return istat
         
-    def hi(self):
-            print(self.sounds[randrange(len(self.sounds))])
-            self.reduce_boredom()
+    def hig(self):
+            print(self.sound[randrang(len(self.sounds))])
+            self.reduce_bore()
         
-    def teach(self,word):
-            self.sounds.append(word)
-            self.reduce_boredom()
+    def iteach(self,wor):
+            self.sound.append(wor)
+            self.reduc_boredom()
         
-    def feed(self):
-            self.reduce_hunger()
+    def ifeed(self):
+            self.reduce_hung()
         
-    def reduce_hunger(self):
+    def ireduce_hunger(self):
             self.hunger=max(0,self.hunger-self.hunger_decrement)
         
-    def reduce_boredom(self):
-            self.boredom=max(0,self.boredom-self.boredom_decrement)
+ 
 
 
-#making a pet and playing with it
+
 
 
 
