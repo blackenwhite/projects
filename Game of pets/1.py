1470dc05a161ff3fc043e9eed678e501f160b302
Nabajyoti Majumdar <52462493+blackenwhite@users.noreply.github.com>
2019-08-04 11:40:29 +0530

    Update 1.py

--- Game of pets/1.py
+++ Game of pets/1.py
@@ -1,13 +1,25 @@
 #class pet implementation
 from random import randrange
 
+p1=Pet("Fid")
+print(p1)
+for i in range(4):
+    p1.clock_tick()
+    print(p1)
+p1.feed()
+p1.hi()
+p1.teach("Boo")
+for i in range(10):
+    p1.hi()
+print(p1)
+
 class Pet(object):
-    boredom_decrement=4
-    hunger_decrement=6
-    boredom_threshold=5
-    hunger_threshold=10
-    sounds=['Mrrp']
-    def __init__(self,name="Kitty"):
+    boredom_dec=4
+    hunger_dec=6
+    boredom_thr=5
+    hunger_thre=10
+    sounds=['Mrr']
+    def __init__(self,name="Bitty"):
         self.name=name
         self.hunger=randrange(self.hunger_threshold)
         self.boredom=randrange(self.boredom_threshold)
@@ -50,86 +62,5 @@ class Pet(object):
 
 #making a pet and playing with it
 
-p1=Pet("Fido")
-print(p1)
-for i in range(10):
-    p1.clock_tick()
-    print(p1)
-p1.feed()
-p1.hi()
-p1.teach("Boo")
-for i in range(10):
-    p1.hi()
-print(p1)
 
-#making a program for non-programmer
 
-import sys
-#sys.setExecutionLimit(60000)
-
-def whichone(petlist,name):
-    for pet in petlist:
-        if pet.name==name:
-            return pet
-    return None
-
-def play():
-    animals=[]
-    
-    option=""
-    base_prompt="""
-      Quit
-      Adopt <petname_with_no_spaces_please>
-      Greet <petname>
-      Teach <petname> <word>
-      Feed <petname>
-      
-      Choice :"""
-    feedback=""
-    while True:
-        action=input(feedback +"\n"+ base_prompt)
-        feedback=""
-        words= action.split()
-        if len(words) >0:
-            command=words[0]
-        else:
-            command=None
-        if command=="Quit":
-            print("Exiting..")
-            return 
-        elif command=="Adopt" and len(words)>1:
-            if whichone(animals,words[1]):
-                feedback+="You already have a pet with that name\n"
-                
-            else:
-                
-                animals.append(Pet(words[1]))
-        elif command=="Greet" and len(words) >1:
-            pet=whichone(animals,words[1])
-            if not pet:
-                feedback+="I didnot recognize that pet name.Please try again\n"
-                print()
-                
-            else:
-                pet.hi()
-        elif command=="Teach" and len(words) >2:
-            pet=whichone(animals,words[1])
-            if not pet:
-                feedback+="I didnot recognize that pet name.Please try again\n"
-            else:
-                pet.teach(words[2])
-                
-        elif command=="Feed" and len(words) >1:
-            pet=whichone(animals,words[1])
-            if not pet:
-                feedback+="I didnot recognize that pet name.Please try again\n"
-            else:
-                pet.feed()
-        else:
-            feedback+="I didnot recognize that pet name.Please try again\n"
-        
-        
-        for pet in animals:
-            pet.clock_tick()
-            feedback+="\n" + pet.__str__()
-play()
